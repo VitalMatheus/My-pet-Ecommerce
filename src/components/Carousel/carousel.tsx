033243427b4b36aa131e83b04b77f04e3360b68e
Matheus <matheusvitalds@gmail.com>
2025-05-14 12:56:29 -0300

    feat: add recommended produts section

--- src/components/Carousel/carousel.tsx
+++ src/components/Carousel/carousel.tsx
@@ -6,17 +6,33 @@ import Image from 'next/image';
 import Autoplay from 'embla-carousel-autoplay'
 
 function Carousel() {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+
+  const scrollPrev = () => emblaApi?.scrollPrev();
+  const scrollNext = () => emblaApi?.scrollNext();
 
   return (
     <div className="w-6xl py-4 m-auto overflow-hidden" ref={emblaRef}>
-      <div className="flex">
+      <div className="flex h-40">
         {data.map((image, index) => (
           <div className="flex-[0_0_100%]" key={index}>
             <Image src={image.img} alt="promotions" width="892" height="344" />
           </div>
-        ))} 
+        ))}
       </div>
+
+      <button
+        onClick={scrollPrev}
+        className="absolute left-4/15 top-1/5 -translate-y-1/2 bg-white shadow px-2 py-1 rounded-full"
+      >
+        ◀
+      </button>
+      <button
+        onClick={scrollNext}
+        className="absolute right-3/15 top-1/5 -translate-y-1/2 bg-white shadow px-2 py-1 rounded-full"
+      >
+        ▶
+      </button>
     </div>
   )
 }
---
+++ src/types/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  old_price: number;
+  image: string;
+  sku: string;
+  type: string;
+  recommended: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
